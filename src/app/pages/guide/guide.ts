04b8b587df18e3c2a84fb0a6a17d300d670958c5
limouzeen <amaratkos@hotmail.com>
2025-11-16 01:03:32 +0700

    add  new guide pdf

--- src/app/pages/guide/guide.ts
+++ src/app/pages/guide/guide.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule, Location } from '@angular/common';
-
+import { FooterStateService } from '../../core/footer-state.service';
 
 @Component({
   selector: 'app-guide',
@@ -9,7 +9,21 @@ import { CommonModule, Location } from '@angular/common';
   styleUrl: './guide.css',
 })
 export class Guide {
-  constructor(private location: Location) {}
+       //  constructor
+  constructor(
+    private footer: FooterStateService,private location: Location
+  ) {}
+   //  ตั้ง threshold เฉพาะหน้า Login: ย่อเมื่อสูง < 719px
+  ngOnInit(): void {
+    this.footer.setThreshold(800);
+    this.footer.setForceCompact(null); // ให้ทำงานแบบ auto ตาม threshold
+  }
+
+  // ออกจากหน้านี้ให้คืนค่ากลับปกติ
+  ngOnDestroy(): void {
+    this.footer.resetAll();
+  }
+
 
   goBack() {
     this.location.back();
